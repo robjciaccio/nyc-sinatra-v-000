4a4e0ef52709b5efa4996013c2855b6dac1646aa
github username <github email address>
2018-02-17 17:49:22 -0500

    Done.

--- config.ru
+++ config.ru
@@ -8,4 +8,6 @@ if defined?(ActiveRecord::Migrator) && ActiveRecord::Migrator.needs_migration?
 end
 
 use LandmarksController
+use TitlesController
+use FiguresController
 run ApplicationController
--- db/migrate/01_create_landmarks.rb
+++ db/migrate/01_create_landmarks.rb
@@ -1,3 +1,10 @@
 class CreateLandmarks < ActiveRecord::Migration
-  raise "Write CreateLandmarks migration here"
+  def up
+    create_table :landmarks do |t|
+      t.string :name
+      t.integer :figure_id
+      t.integer :year_completed
+      t.timestamps
+    end
+  end
 end
